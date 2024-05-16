0dbda48f44b922a9c62072fa8e02766151090b78
XYCode Kerman <xycode-xyc@outlook.com>
2024-05-16 21:07:46 +0800

    perf: 使用lazy检查权限信息

--- utils/permission.ts
+++ utils/permission.ts
@@ -1,4 +1,4 @@
 export function checkPermission(resource: string, action: string): Ref<boolean> {
-    const resp = useFetch(`/api/user/check?resource=${resource}&action=${action}`)
+    const resp = useFetch(`/api/user/check?resource=${resource}&action=${action}`, { lazy: true })
     return resp.data as Ref<boolean>
 }

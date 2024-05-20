d173b018f549b7602e977e0f3fb4caf657c86a9d
XYCode Kerman <xycode-xyc@outlook.com>
2024-05-20 22:48:52 +0800

    refactor: 页脚兼容移动端

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -7,7 +7,7 @@
 
             <div class="my-1 h-[2px] bg-gray-200"></div>
 
-            <CardContent class="grid grid-cols-3 text-xs text-gray-600 py-2 gap-8">
+            <CardContent class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-xs text-gray-600 py-2 gap-8">
                 <!-- 栏1 项目地址等信息 -->
                 <div class="flex flex-col">
                     <h1 class="text-base font-extrabold mb-1">About ASMRE</h1>

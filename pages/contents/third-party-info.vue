427cb2229af160848c43ad81b3798123a537a26c
XYCode Kerman <xycode-xyc@outlook.com>
2024-05-20 22:53:33 +0800

    refactor: 三大内容页面兼容移动端

--- pages/contents/third-party-info.vue
+++ pages/contents/third-party-info.vue
@@ -3,7 +3,8 @@ const { data } = await useAsyncData('page-data', () => queryContent('/third-part
 </script>
 
 <template>
-    <main class="prose max-w-none mx-32">
+    <main
+        class="prose max-w-none mx-8 md:mx-16 lg:mx-32 prose-code:text-xs md:prose-code:text-sm lg:prose-code:text-base">
         <ContentRenderer :value="data">
             <h1>{{ data.title }}</h1>
             <ContentRendererMarkdown :value="data" />
